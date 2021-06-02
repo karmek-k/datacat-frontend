5668c173874903320ea4c76acc53f038a396a2eb
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-02 19:44:47 +0200

    move to a different machine

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//materialUI
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+}));
 
 const LoginPage: React.FC = () => {
+    const classes = useStyles();
 
+    const [loginData, setLoginData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        console.log("handle submit");
+    }
+    
     return(
-        
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h2" variant="h5">
+                    Login
+                </Typography>
+                <form className={classes.form}>
+                    <TextField 
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                </form>
+            </div>
+        </Container>
     );
 
 };
