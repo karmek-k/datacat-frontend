31fd02df6e44465f9e48202f5b193b67878adc05
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-12 10:08:55 +0200

    init new branch

---
+++ src/components/dashboard/JobCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+interface Props {
+
+};
+
+
+const JobCard: React.FC<Props> = ({}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default JobCard;
