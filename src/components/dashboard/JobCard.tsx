4432c6c348ab4ebb648be1c81479c98ee1855009
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-12 11:03:18 +0200

    change interface, add typography

--- src/components/dashboard/JobCard.tsx
+++ src/components/dashboard/JobCard.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 interface Props {
-
+    name: String;
+    url: String;
+    thumbnail: String;
 };
 
+const useStyles = makeStyles({
+    root: {
+        minWidth: 300,
+    },
+})
 
-const JobCard: React.FC<Props> = ({}) => {
+
+const JobCard: React.FC<Props> = ({name, url, thumbnail}) => {
+    const classes = useStyles();
+
     return (
-        <div>
-
-        </div>
+        <Card className={classes.root} variant="outlined">
+            <CardContent>
+                <Typography component="h5" variant="h5">
+                    {name}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
 
