aa5791353feb00f6031888de4fba885298fefa7f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-10 18:52:42 +0200

    Create and provide a query client

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { QueryClient } from 'react-query';
+
+export const queryClient = new QueryClient();
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL
