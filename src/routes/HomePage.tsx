08c7ad96ff8e15b92eb6c4c538ce708acced0795
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-10 18:57:20 +0200

    Place a register button on the home page

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,11 +1,20 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
 import Layout from '../components/shared/Layout';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
-      <Button color="primary" variant="contained">
+      <Button
+        color="primary"
+        variant="contained"
+        component={Link}
+        to="/register"
+      >
+        Register
+      </Button>
+      <Button color="primary" variant="outlined" component={Link} to="/login">
         Log in
       </Button>
     </Layout>
