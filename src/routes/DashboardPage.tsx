6e9b15fe661ab893ebba0f5866f3b184e5b5a02e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-15 10:34:59 +0200

    Handle errors and loading on the dashboard page

--- src/routes/DashboardPage.tsx
+++ src/routes/DashboardPage.tsx
@@ -1,23 +1,39 @@
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import DownJobsAlert from '../components/dashboard/DownJobsAlert';
 import Layout from '../components/shared/Layout';
 import useJobs from '../hooks/dashboard/useJobs';
+import { JobStatus } from '../interfaces/resources/jobs';
 
 const DashboardPage: React.FC = () => {
   const jobs = useJobs();
 
+  if (jobs.isLoading || !jobs.data) {
+    return (
+      <Layout>
+        <Typography>Loading...</Typography>
+      </Layout>
+    );
+  }
+
+  if (jobs.isError) {
+    return (
+      <Layout>
+        <Typography color="error">Error: {jobs.error}</Typography>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      {jobs.data ? (
-        <>
-          <DownJobsAlert downCount={1} />
-          {jobs.data.map(job => (
-            <p key={job.id}>{job.jobName}</p>
-          ))}
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <DownJobsAlert
+        downCount={
+          jobs.data.filter(job => job.status === JobStatus.DOWN).length
+        }
+      />
+      {jobs.data.map(job => (
+        <Typography key={job.id}>{job.jobName}</Typography>
+      ))}
     </Layout>
   );
 };
